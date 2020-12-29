f93755e735b6ecc9d0b549fad8e20e2075d4077d
Zyq612 <68604343+Zyq612@users.noreply.github.com>
2020-12-29 23:11:27 +0800

    Update app.js

--- 2018/app.js
+++ 2018/app.js
@@ -93,7 +93,7 @@ function race(candidates) {
       <span class="player">${p[1]} ${i + 1}</span>
     </div>`;
   }).join('');
-  window.alert(5 + 6);
+  <p id="demo">我的第一个段落。</p>
   const round1 = partRace(durations, 0.25);
   const round2 = partRace(durations, 0.33);
   const round3 = partRace(durations, 0.5);
